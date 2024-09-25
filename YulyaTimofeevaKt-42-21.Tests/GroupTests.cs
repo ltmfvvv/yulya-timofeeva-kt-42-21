29a0817c1e731105a731316fcb32dbad9cb92028
yulya-timofeeva-kt-42-21 <yulyatim23@yandex.ru>
2024-09-25 11:51:42 +0300

    feat: add GroupTests

---
+++ YulyaTimofeevaKt-42-21.Tests/GroupTests.cs
@@ -0,0 +1,24 @@
+using YulyaTimofeevaKt_42_21.Models;
+
+namespace YulyaTimofeevaKt_42_21.Tests
+
+{
+    public class GroupTests
+    {
+        [Fact]
+        public void IsValidGroupName_KT4221_True()
+        {
+            //arrange
+            var testGroup = new Group
+            {
+                GroupName = "KT-42-21"
+            };
+
+            //act
+            var result = testGroup.IsValidGroupName();
+
+            //assert
+            Assert.True(result);
+        }
+    }
+}
--- YulyaTimofeevaKt-42-21/Models/Group.cs
+++ YulyaTimofeevaKt-42-21/Models/Group.cs
@@ -1,8 +1,14 @@
-﻿namespace YulyaTimofeevaKt_42_21.Models
+﻿using System.Text.RegularExpressions;
+
+namespace YulyaTimofeevaKt_42_21.Models
 {
     public class Group
     {
         public int GroupId { get; set; }
         public string GroupName { get; set; }
+        public bool IsValidGroupName()
+        {
+            return Regex.Match(GroupName, @"/\D*-\d*-\d\d/g").Success;
+        }
     }
 }
